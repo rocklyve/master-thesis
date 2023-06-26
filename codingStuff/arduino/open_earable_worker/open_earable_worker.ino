416b2d737dfb163b0f45f810d3604bbdaa53b983
David Laubenstein <david.laubenstein@jamitlabs.com>
2023-06-26 10:59:32 +0200

    Add IMU data tracking

--- codingStuff/arduino/open_earable_worker/open_earable_worker.ino
+++ codingStuff/arduino/open_earable_worker/open_earable_worker.ino
@@ -2,6 +2,7 @@
 #include "Protocentral_MLX90632.h"
 #include "TCA9548A.h"
 #include "SD_Logger.h"
+#include "IMU_Sensor.h"
 
 TCA9548A mux;
 Protocentral_MLX90632 mlx[5];
@@ -11,6 +12,7 @@ bool isDebugMode = false;
 int counter = 0;
 
 SD_Logger * logger;
+IMU_Sensor imu;
 
 /*****************************************  setup() *************************************************/
 void setup() {
@@ -21,6 +23,7 @@ void setup() {
   Wire.begin();
   // Initialize the multiplexer
   mux.begin();
+  imu.start();
 
   // Select the multiplexer channel for each sensor
   for (uint8_t i = 0; i < 5; i++) {
@@ -35,28 +38,23 @@ void setup() {
     }
   }
 
-  // SD SETUP
-  Serial.println("Start setup");
-
   logger = new SD_Logger();
-  
   // logger->set_name("data.csv");  // Set the name of the log file
   if (!logger->begin()) {
     Serial.println("SD Logger initialization failed!");
     while (1);  // Stop execution if SD Logger initialization fails
-  } else{ 
-  Serial.println("set csv name"); 
-
-  Serial.println("Logger initialized and CSV name set.");
+  } else{
+    Serial.println("Logger initialized and CSV name set.");
   }  
 }
 
 /*****************************************  loop() *************************************************/
-void loop() {  
-  int data[6];
-  data[0] = 5;  // Number of data elements
+void loop() { 
+  int amount_of_data_columns = 5 + 9; 
+  int data[amount_of_data_columns + 1];
+  data[0] = amount_of_data_columns;  // Number of data elements
 
-  if (counter == 200) {
+  if (counter == 1000) {
     unsigned int timestamp = millis();
     logger->data_callback(-1, timestamp, (uint8_t*)data);
     // Logging the data
@@ -69,16 +67,30 @@ void loop() {
       mux.closeAll();
       mux.openChannel(MLX_CHANNELS[i]);
       
-      float temperature = mlx[i].getObjectTemp();
-      data[i+1] = temperature * 100;
-
-      // Serial.print("Sensor ");
-      // Serial.print(i);
-      // Serial.print(" Temperature: ");
-      // Serial.print(temperature);
-      // Serial.print(" °C, counter: ");
-      // Serial.println(counter);
+      data[i+1] = mlx[i].getSensorTemp() * 100;
     }
+    // now store IMU data
+    float accelX, accelY, accelZ;
+    imu.get_acc(accelX, accelY, accelZ);
+    data[6] = accelX * 100;
+    data[7] = accelY * 100;
+    data[8] = accelZ * 100;
+
+    // Get gyroscope data
+    float gyroX, gyroY, gyroZ;
+    imu.get_gyro(gyroX, gyroY, gyroZ);
+    data[9] = gyroX * 100;
+    data[10] = gyroY * 100;
+    data[11] = gyroZ * 100;
+
+    // Get magnetometer data
+    float magX, magY, magZ;
+    imu.get_mag(magX, magY, magZ);
+    data[12] = magX * 100;
+    data[13] = magY * 100;
+    data[14] = magZ * 100;
+    
+    // now save to SD card
     unsigned int timestamp = millis();
     Serial.print("Data: ");
     Serial.print(data[1]);
@@ -89,8 +101,27 @@ void loop() {
     Serial.print(", ");
     Serial.print(data[4]);
     Serial.print(", ");
-    Serial.print(data[5]);
-    Serial.println("!");
+    Serial.println(data[5]);
+    Serial.print("IMU [");
+    Serial.print(data[6]);
+    Serial.print(", ");
+    Serial.print(data[7]);
+    Serial.print(", ");
+    Serial.print(data[8]);
+    Serial.print(", ");
+    Serial.print(data[9]);
+    Serial.print(", ");
+    Serial.print(data[10]);
+    Serial.print(", ");
+    Serial.print(data[11]);
+    Serial.print(", ");
+    Serial.print(data[12]);
+    Serial.print(", ");
+    Serial.print(data[13]);
+    Serial.print(", ");
+    Serial.print(data[14]);
+    Serial.println("]");
+    Serial.println("");
     logger->data_callback(1, timestamp, (uint8_t*) data);
   }
 }
