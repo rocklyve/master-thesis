ea12104be3252179f906388dc75d926113a47d7a
David Laubenstein <david.laubenstein@jamitlabs.com>
2023-09-04 09:26:30 +0200

    Refactor python code to classes

---
+++ codingStuff/studyAnalysis/pre_study_calculations/pre_calculations.py
@@ -0,0 +1,7 @@
+# Description: This file contains the pre-calculations for the study
+class PreCalculations:
+    def __init__(self):
+        self.__init__()
+
+if __name__ == "__main__":
+    print("PreCalculations")
