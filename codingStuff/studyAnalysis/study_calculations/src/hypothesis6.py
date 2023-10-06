8e4c294c6e5b892dd68edd75fed8c26f3a953798
David Laubenstein <david.laubenstein@jamitlabs.com>
2023-10-06 09:43:13 +0200

    Hypothesis6 better curves

--- codingStuff/studyAnalysis/study_calculations/src/hypothesis6.py
+++ codingStuff/studyAnalysis/study_calculations/src/hypothesis6.py
@@ -10,73 +10,90 @@ class Hypothesis6Analyzer:
     def calculate_relative_change(self, series):
         return np.abs(series.pct_change() * 100)
 
-    def analyze_all_participants(self, target_folder):
-        # Initialize accumulators for each sensor and for movement
-        sensor_data_accum = {sensor: [] for sensor in self.all_calib_data[0].temp_columns}
-        movement_data_accum = []
-
-        for calib in self.all_calib_data:
-            for sensor in calib.temp_columns:
-                sensor_data = calib.raw_data[sensor].dropna().reset_index(drop=True)
-                rel_change = self.calculate_relative_change(sensor_data)
-                sensor_data_accum[sensor].append(rel_change)
-
-            imu_columns = ['ACC_X', 'ACC_Y', 'ACC_Z', 'GYRO_X', 'GYRO_Y', 'GYRO_Z', 'MAG_X', 'MAG_Y', 'MAG_Z']
-            movement_data = np.sqrt((calib.raw_data[imu_columns] ** 2).sum(axis=1)).dropna().reset_index(drop=True)
-            movement_data_accum.append(movement_data)
-
-        # Take the average across all participants
-        avg_sensor_data = {sensor: pd.concat(sensor_data_accum[sensor], axis=1).mean(axis=1).abs() for sensor in
-                           sensor_data_accum}
-        avg_movement_data = pd.concat(movement_data_accum, axis=1).mean(axis=1)
-
-        # Plot the average data
-        fig, axs = plt.subplots(len(avg_sensor_data) + 1, 1, figsize=(15, 20))
-
-        for i, (sensor, data) in enumerate(avg_sensor_data.items()):
-            axs[i].plot(data.index, data)
-            axs[i].set_title(f"Average Rel Change {sensor}")
-            axs[i].set_xlabel('Time (min)')
-            axs[i].set_ylabel('Relative Change (%)')
-
-        axs[-1].plot(avg_movement_data.index, avg_movement_data, alpha=0.3)
-        axs[-1].set_title('Average Movement')
-        axs[-1].set_xlabel('Time (min)')
-        axs[-1].set_ylabel('Movement Magnitude')
-
-        plt.tight_layout()
-        plt.savefig(f"{target_folder}/hypothesis6_all_participants.png")
-        plt.close()
+    def calculate_movement(self, df, imu_columns):
+        return np.sqrt((df[imu_columns] ** 2).sum(axis=1))
 
     def analyze(self, target_folder):
         for i, calib in enumerate(self.all_calib_data):
-            fig, axs = plt.subplots(len(calib.temp_columns) + 1, 1, figsize=(15, 20))
+            fig, axes = plt.subplots(7, 1, figsize=(8.27, 11.7), sharex=True)
 
-            # Loop over each sensor to plot its relative change
+            # Filter data for phase 2 and 3
+            filtered_data = calib.raw_data[(calib.raw_data['ID'] == 2) | (calib.raw_data['ID'] == 3)]
+
+            # Calculate relative change for each temperature sensor
             for j, sensor in enumerate(calib.temp_columns):
-                sensor_data = calib.raw_data[sensor].dropna().reset_index(drop=True)
-                timestamps = calib.raw_data.loc[sensor_data.index, 'TIMESTAMP']
+                sensor_data = filtered_data[sensor].dropna().reset_index(drop=True)
                 rel_change = self.calculate_relative_change(sensor_data)
-                axs[j].plot(timestamps, rel_change)
-                axs[j].set_title(f"Rel Change {sensor}")
-                axs[j].set_xlabel('Time (min)')
-                axs[j].set_ylabel('Relative Change (%)')
+                axes[j].plot(rel_change, label=f"Rel Change {sensor}")
+                axes[j].set_title(f"Rel Change {sensor}")
 
-                # Add vertical lines for phase changes
-                phase_change_indices = calib.raw_data['ID'].diff().ne(0)
-                phase_change_times = calib.raw_data.loc[phase_change_indices, 'TIMESTAMP']
-                for phase_change_time in phase_change_times:
-                    axs[j].axvline(x=phase_change_time, color='r', linestyle='--')
-
-            # Plot the movement in the last subplot
+            # Calculate movement
             imu_columns = ['ACC_X', 'ACC_Y', 'ACC_Z', 'GYRO_X', 'GYRO_Y', 'GYRO_Z', 'MAG_X', 'MAG_Y', 'MAG_Z']
-            movement_data = np.sqrt((calib.raw_data[imu_columns] ** 2).sum(axis=1)).dropna().reset_index(drop=True)
-            axs[-1].plot(movement_data.index, movement_data, alpha=0.3)
-            axs[-1].set_title('Movement')
-            axs[-1].set_xlabel('Time (min)')
-            axs[-1].set_ylabel('Movement Magnitude')
+            movement = self.calculate_movement(filtered_data, imu_columns)
+            axes[-1].plot(movement, label='Movement', alpha=0.3)
+            axes[-1].set_title('Movement')
 
-            # Save the figure
-            plt.tight_layout()
+            plt.suptitle(f"Participant {i + 1}")
             plt.savefig(f"{target_folder}/hypothesis6_participant_{i + 1}.png")
             plt.close()
+
+    def analyze_all_participants(self, target_folder):
+        avg_rel_changes = {}
+        avg_movement = None
+
+        for calib in self.all_calib_data:
+            # Filter data for Phase 2 and Phase 3 (ID == 2 or ID == 3)
+            filtered_data = calib.raw_data[calib.raw_data['ID'].isin([2, 3])]
+
+            # Calculate movement
+            imu_columns = ['ACC_X', 'ACC_Y', 'ACC_Z', 'GYRO_X', 'GYRO_Y', 'GYRO_Z', 'MAG_X', 'MAG_Y', 'MAG_Z']
+            movement_data = self.calculate_movement(filtered_data, imu_columns)
+
+            # Sync movement and temperature data
+            synced_data = self.sync_data(pd.DataFrame({'TIMESTAMP': filtered_data['TIMESTAMP'], 'Movement': movement_data}),
+                                         filtered_data)
+
+            # Calculate and accumulate the average relative change for each sensor
+            for sensor in calib.temp_columns:
+                rel_change = self.calculate_relative_change(synced_data[sensor].dropna())
+                if sensor not in avg_rel_changes:
+                    avg_rel_changes[sensor] = []
+                avg_rel_changes[sensor].append(rel_change)
+
+            # Calculate and accumulate the average movement
+            if avg_movement is None:
+                avg_movement = movement_data
+            else:
+                avg_movement += movement_data
+
+        # Calculate the final averages
+        for sensor in avg_rel_changes:
+            avg_rel_changes[sensor] = pd.concat(avg_rel_changes[sensor], axis=1).mean(axis=1)
+        avg_movement = avg_movement / len(self.all_calib_data)
+
+        # Plotting
+        fig, axs = plt.subplots(len(calib.temp_columns) + 1, 1, figsize=(8.27, 11.69))  # A4 size
+
+        for i, (sensor, rel_change) in enumerate(avg_rel_changes.items()):
+            axs[i].plot(rel_change.index, rel_change, label=f'Avg Rel Change {sensor}')
+            axs[i].set_ylabel('Rel Change (%)')
+            axs[i].legend()
+
+        axs[-1].plot(avg_movement.index, avg_movement, label='Avg Movement', alpha=0.3)
+        axs[-1].set_xlabel('Time (minutes)')
+        axs[-1].set_ylabel('Movement')
+        axs[-1].legend()
+
+        plt.tight_layout()
+        plt.savefig(f"{target_folder}/hypothesis6_all_participants.png")
+        plt.close(fig)
+
+    # Syncing the movement and temperature data based on their timestamps
+    def sync_data(self, movement_data, temperature_data):
+        # Assuming the TIMESTAMP column is in minutes in both dataframes
+        synced_data = pd.merge_asof(temperature_data.sort_values('TIMESTAMP'),
+                                    movement_data.sort_values('TIMESTAMP'),
+                                    on='TIMESTAMP',
+                                    direction='nearest')
+        return synced_data
+
